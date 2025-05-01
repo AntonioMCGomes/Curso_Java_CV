5d815fdc5a7408364f4d4940e3f0ca582b6ef752
AntonioMCGomes <marxmarc@gmail.com>
2025-05-01 20:34:24 +0100

    Curso de Java

---
+++ index.java
@@ -0,0 +1,37 @@
+/*
+ * Um código executável em Java chama-se Bytecode
+ * 
+ * O código será enviado para o compilador JavaC
+ * 
+ * Em seguida, o Bytecode passa por uma JVM (Java Virtual Machine) para ser transformado em linguagem de máquina.
+ * 
+ * (Para cada plataforma, há uma JVM)
+ * 
+ * O código da JVM será compreendido em qualquer plataforma.
+ * 
+ * Essa prática tem o nome de WORA (Write once run anywhere)
+ * 
+ * JRE Java Runtime Enviroment - Execução de Java
+ *  - JVM
+ *      * Loader
+ *      * Verificador
+ *      * Interpretador
+ *      * Gerenciador de memória      
+ *      * Compilador JIT (Jus in time/Tradução em tempo real)
+ * 
+ *  - Bibliotecas
+ *      * Apis para utilizar
+ * 
+ * JDK Java Development Kit - Desenvolvimento de Java
+ *  - Há um JRE
+ *  - JavaLang (A linguagem Java)
+ *  - JavaTools
+ *      *JavaC
+ *      *Debugger
+ *      *APIs
+ *  - IDE
+ * 
+ */
+
+
+ 
